d8426fb17dd36d1adf0fcbc6ec19b8aef0108c7e
aline-bebe <ingabirealine40@gmail.com>
2019-07-26 16:44:26 +0200

    mousemove

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
- $(document).ready(function(){
-    $('#img1').click(function(){
-         $('.p1').show();
-         $('#img1').hide();
-            
-     });
-     $('.p1').click(function(){
-        $('#img1').show();
-        $('.p1').hide();;
- });
+    $(document).ready(function(){
+        $('#img1').click(function(){
+            $('.p1').show();
+            $('#img1').hide();
+                
+        });
+        $('.p1').click(function(){
+            $('#img1').show();
+            $('.p1').hide();;
+    });
 
- $('#img2').click(function(){
-    $('.p2').show();
-    $('#img2').hide();
-       
-});
-$('.p2').click(function(){
-   $('#img2').show();
-   $('.p2').hide();;
-});
+    $('#img2').click(function(){
+        $('.p2').show();
+        $('#img2').hide();
+        
+    });
+    $('.p2').click(function(){
+    $('#img2').show();
+    $('.p2').hide();;
+    });
 
-$('#img3').click(function(){
-    $('.p3').show();
-    $('#img3').hide();
-       
-});
-$('.p3').click(function(){
-   $('#img3').show();
-   $('.p3').hide();;
-});
+    $('#img3').click(function(){
+        $('.p3').show();
+        $('#img3').hide();
+        
+    });
+    $('.p3').click(function(){
+    $('#img3').show();
+    $('.p3').hide();;
+    });
 
+        });
 
-     });
+        
